4341f639e8033162d4501e5ad4e3b31dfe963f58
Silas Caimi <silascaimi@live.com>
2020-08-04 17:44:46 -0300

    create react component PageHeader

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
 function TeacherList() {
-  return <h1>TeacherList</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis" />
+    </div>
+  );
 }
 
 export default TeacherList;
