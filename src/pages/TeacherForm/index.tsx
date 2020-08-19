dbef26bdb2415744f3976d4d69869b38b2e82840
Silas Caimi <silascaimi@live.com>
2020-08-19 13:25:07 -0300

    fix teacherform select label

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -82,7 +82,7 @@ function TeacherForm() {
                     { value: '1', label: 'Segunda-feira' },
                     { value: '2', label: 'Terça-feira' },
                     { value: '3', label: 'Quarta-feira' },
-                    { value: '4 Física', label: 'Quinta-feira' },
+                    { value: '4', label: 'Quinta-feira' },
                     { value: '5', label: 'Sexta-feira' },
                     { value: '6', label: 'Sábado' },
                   ]}
